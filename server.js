1cef0887412b9c7300f1b7dd201cff649b08c349
thanhyendang <nino29042004@gmail.com>
2025-04-16 09:04:46 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'public'))); // 'public' là thư mục chứa các file HTML/CSS.
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
